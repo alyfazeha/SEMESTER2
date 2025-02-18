bcf48f7b8caf8a1281eecf29880e187af352ebfb
alyfazeha <alyfazeha@gmail.com>
2025-02-18 17:19:55 +0700

    jawaban nomor 2

---
+++ JOBSHEET2/Dosen05.java
@@ -0,0 +1,43 @@
+package JOBSHEET2;
+public class Dosen05 {
+    String idDosen;
+    String nama;
+    boolean statusAktif;
+    int tahunBergabung;
+    String bidangKeahlian;
+
+    public Dosen05() {
+
+    }
+    public Dosen05(String id, String nm, boolean sttsAkt, int thnGbg, String bidAhli) {
+        idDosen = id;
+        nama = nm;
+        statusAktif = sttsAkt;
+        tahunBergabung = thnGbg;
+        bidangKeahlian = bidAhli;
+    }
+
+    void tampilInformasi() {
+        System.out.println();
+        System.out.println("Id Dosen : " + idDosen);
+        System.out.println("Nama Dosen : " + nama);
+        System.out.println("Status Dosen : " + statusAktif);
+        System.out.println("Tahun Bergabung : " + tahunBergabung);
+        System.out.println("Bidang Keahlian : " + bidangKeahlian);
+    }
+    void setStatusAktif( boolean status ) {
+        statusAktif = status;
+        if (statusAktif = true) {
+            System.out.println("Dosen Aktif!!");
+        } else {
+            System.out.println("Dosen Non-Aktif!!");
+        }
+    }
+    int hitungMasaKerja(int thnSkrg) {
+        int tahunMasa = thnSkrg - tahunBergabung;
+        return tahunMasa;
+    }
+    void ubahKeahlian(String bidang) {
+        bidangKeahlian = bidang;
+    }
+}
---
+++ JOBSHEET2/DosenMain05.java
@@ -0,0 +1,22 @@
+package JOBSHEET2;
+public class DosenMain05 {
+    public static void main(String[] args) {
+        Dosen05 alyfa = new Dosen05();
+        alyfa.idDosen = "AL001";
+        alyfa.nama = "Alyfa Zahra";
+        alyfa.statusAktif = true;
+        alyfa.tahunBergabung = 2003;
+        alyfa.bidangKeahlian = "Matematika";
+
+        alyfa.tampilInformasi();
+        alyfa.setStatusAktif(false);
+        // int masa = alyfa.hitungMasaKerja(2025);
+        System.out.println("Masa Kerja : " + alyfa.hitungMasaKerja(2025));
+        alyfa.ubahKeahlian("Sains");
+        alyfa.tampilInformasi();
+
+        Dosen05 zahra = new Dosen05("ZH002", "Zahra rarara", true, 2017, "Komputer");
+        zahra.tampilInformasi();
+
+    }
+}
