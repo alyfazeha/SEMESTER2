68f99e2c95fa62f2c718074bad857d0eabf82b29
alyfazeha <alyfazeha@gmail.com>
2025-02-18 14:43:12 +0700

    selesai soal percobaan 1

--- JOBSHEET2/Mahasiswa05.java
+++ JOBSHEET2/Mahasiswa05.java
@@ -32,14 +32,19 @@ public class Mahasiswa05 {
     }
 
     String nilaiKinerja() {
-        if (ipk >= 3.5) {
-            return "Kinerja sangat baik";
-        } else if (ipk >= 3.0) {
-            return "Kinerja baik";
-        } else if (ipk >= 2.0) {
-            return "Kinerja cukup";
+        if (ipk >= 0.0 && ipk <= 4.0) {
+            if (ipk >= 3.5) {
+                return "Kinerja sangat baik";
+            } else if (ipk >= 3.0) {
+                return "Kinerja baik";
+            } else if (ipk >= 2.0) {
+                return "Kinerja cukup";
+            } else {
+                return "Kinerja kurang";
+            }
         } else {
-            return "Kinerja kurang";
+            return "IPK tidak valid. Harus antara 0.0 dan 4.0";
         }
+        
     }
 }
