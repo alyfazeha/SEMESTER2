0f0ebf44391d99ba01b85594d9b86f2d1456fd23
alyfazeha <alyfazeha@gmail.com>
2025-03-18 13:11:07 +0700

    practicum2

---
+++ JOBSHEET6/MahasiwaBerprestasi05.java
@@ -0,0 +1,33 @@
+package JOBSHEET6;
+public class MahasiwaBerprestasi05 {
+    Mahasiswa05[] listMhs = new Mahasiswa05[5];
+    int idx;
+
+    void tambah (Mahasiswa05 m) {
+        if (idx < listMhs.length) {
+            listMhs[idx] = m;
+            idx++;
+        } else {
+            System.out.println("data sudah penuh");
+        }
+    }
+
+    void tampil() {
+        for (Mahasiswa05 m:listMhs) {
+            m.tampilInformasi();
+            System.out.println("------------------------------");
+        }
+    }
+
+    void bubbleSort() {
+        for (int i = 0; i < listMhs.length-1; i++) {
+            for (int j = 1; j < listMhs.length-i; j++) {
+                if (listMhs[j].ipk > listMhs[j-1].ipk) {
+                    Mahasiswa05 tmp = listMhs[j];
+                    listMhs[j] = listMhs[j-1];
+                    listMhs[j-1] = tmp;
+                }
+            }
+        }
+    }
+}
