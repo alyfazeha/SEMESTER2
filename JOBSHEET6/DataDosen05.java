6a011a6eb72c241bd83d77b1d3e42f85a7d2fc89
alyfazeha <alyfazeha@gmail.com>
2025-03-25 16:51:40 +0700

    final

--- JOBSHEET6/DataDosen05.java
+++ JOBSHEET6/DataDosen05.java
@@ -1,7 +1,11 @@
 package JOBSHEET6;
 public class DataDosen05 {
-    Dosen05[] dataDosen = new Dosen05[10];
+    Dosen05[] dataDosen;
     int idx;
+
+    public DataDosen05(int jumlah) {
+        dataDosen = new Dosen05[jumlah];
+    }
 
     void tambah(Dosen05 dsn) {
         if (idx < dataDosen.length) {
@@ -55,5 +59,94 @@ public class DataDosen05 {
             }
             dataDosen[j] = temp;
         }
+    }
+
+    void pencarianDataSequential(String cariNama) {
+        int jumlahHasil = 0;
+        for (int i = 0; i < dataDosen.length; i++) {
+            if (dataDosen[i].nama.equalsIgnoreCase(cariNama)) {
+                System.out.println("Data dosen dengan nama : " + dataDosen[i].nama + 
+                                   " ditemukan pada indeks " + i);
+                System.out.println("Dengan detail data dosen : ");
+                System.out.println("Kode          : " + dataDosen[i].kode);
+                System.out.println("Nama          : " + dataDosen[i].nama);
+                    if (dataDosen[i].jenisKelamin == true) {
+                        System.out.println("Jenis Kelamin : Wanita");
+                    } else {
+                        System.out.println("Jenis Kelamin : Pria");
+                    }
+                System.out.println("Usia          : " + dataDosen[i].usia);
+                System.out.println();
+                jumlahHasil++;
+            }
+        }
+        if (jumlahHasil == 0) {
+            System.out.println("Data tidak ditemukan.");
+        } else {
+            System.out.println("===============================================");
+            System.out.println("PERINGATAN : Data ditemukan sebanyak " + jumlahHasil);
+            System.out.println("===============================================");
+            if (jumlahHasil > 1) {
+                System.out.println("PERINGATAN: Terdapat lebih dari satu data dosen dengan nama tersebut.");
+            }
+        }
+    }
+
+    void pencarianDataBinary(int cari, int left, int right) {
+        int mid;
+        int jumlahHasil = 0; 
+        int[] indexSama = new int[dataDosen.length]; 
+        
+        while (right >= left) {
+            mid = (left + right) / 2;
+            
+            if (cari == dataDosen[mid].usia) {
+                indexSama[jumlahHasil] = mid;
+                jumlahHasil++;
+                
+                int temp = mid - 1;
+                while (temp >= left && dataDosen[temp].usia == cari) {
+                    indexSama[jumlahHasil] = temp;
+                    jumlahHasil++;
+                    temp--;
+                }
+                
+                temp = mid + 1;
+                while (temp <= right && dataDosen[temp].usia == cari) {
+                    indexSama[jumlahHasil] = temp;
+                    jumlahHasil++;
+                    temp++;
+                }
+                
+                System.out.println("===============================================");
+                System.out.println("PERINGATAN : Data ditemukan sebanyak " + jumlahHasil);
+                System.out.println("===============================================");
+                for (int i = 0; i < jumlahHasil; i++) {
+                    int index = indexSama[i];
+                    System.out.println("Data dosen dengan usia : " + dataDosen[index].usia + 
+                                       " ditemukan pada indeks " + index);
+                    System.out.println("Dengan detail data dosen : ");
+                    System.out.println("Kode          : " + dataDosen[index].kode);
+                    System.out.println("Nama          : " + dataDosen[index].nama);
+                    if (dataDosen[index].jenisKelamin == true) {
+                        System.out.println("Jenis Kelamin : Wanita");
+                    } else {
+                        System.out.println("Jenis Kelamin : Pria");
+                    }
+                    System.out.println("Usia          : " + dataDosen[index].usia);
+                    System.out.println();
+                }
+
+                if (jumlahHasil > 1) {
+                    System.out.println("PERINGATAN: Terdapat lebih dari satu data dosen dengan usia tersebut.");
+                }
+                return; 
+            } else if (dataDosen[mid].usia < cari) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        System.out.println("Data tidak ditemukan.");
     }
 }
--- JOBSHEET6/DosenDemo05.java
+++ JOBSHEET6/DosenDemo05.java
@@ -2,10 +2,13 @@ package JOBSHEET6;
 import java.util.Scanner;
 public class DosenDemo05 {
     public static void main(String[] args) {
-        
         Scanner input = new Scanner(System.in);
-            DataDosen05 dosen = new DataDosen05();
-            for (int i = 0; i < 10; i++) {
+
+        System.out.print("Masukkan jumlah dosen : ");
+        int jumlah = input.nextInt();
+
+        DataDosen05 dosen = new DataDosen05(jumlah);
+            for (int i = 0; i < jumlah; i++) {
                 System.out.println("Masukkan jumlah data dosen ke-" + (i+1));
                 System.out.print("Masukkan Kode          : ");
                 String kode = input.next();
@@ -25,20 +28,40 @@ public class DosenDemo05 {
                 dosen.tambah(dsn);
                 System.out.println();
             }
-    
-            System.out.println("Data dosen sebelum sorting : ");
+
+            System.out.println("-----------------------------------------");
+            System.out.println("Data dosen sebelum sorting ");
+            System.out.println("-----------------------------------------");
             dosen.tampil();
 
-            System.out.println("Data dosen setelah sorting menggunakan BUBBLE SORT (ASC) : ");
+            System.out.println("-----------------------------------------");
+            System.out.println("Searching menggunakan Sequential Search");
+            System.out.println("-----------------------------------------");
+            System.out.print("Masukkan nama dosen yang dicari : ");
+            String cariNama = input.nextLine();
+            dosen.pencarianDataSequential(cariNama);
+        
+            System.out.println("-----------------------------------------");
+            System.out.println("Searching menggunakan Binary Search");
+            System.out.println("-----------------------------------------");
+            System.out.print("Masukkan usia yang dicari : ");
+            int cari = input.nextInt();
             dosen.SortingASC();
-            dosen.tampil();
-    
-            System.out.println("Data dosen setelah sorting menggunakan SELECTION SORT (DSC) : ");
-            dosen.SortingDSC();
-            dosen.tampil();
+            dosen.pencarianDataBinary(cari, 0, jumlah-1);
 
-            System.out.println("Data dosen setelah sorting menggunakan INSERTION SORT (DSC) : ");
-            dosen.insertion();
-            dosen.tampil();
+            // System.out.println("Data dosen sebelum sorting : ");
+            // dosen.tampil();
+
+            // System.out.println("Data dosen setelah sorting menggunakan BUBBLE SORT (ASC) : ");
+            // dosen.SortingASC();
+            // dosen.tampil();
+    
+            // System.out.println("Data dosen setelah sorting menggunakan SELECTION SORT (DSC) : ");
+            // dosen.SortingDSC();
+            // dosen.tampil();
+
+            // System.out.println("Data dosen setelah sorting menggunakan INSERTION SORT (DSC) : ");
+            // dosen.insertion();
+            // dosen.tampil();
     }
 }
