dd8260faad87f9b93bb725d12960d6c2ce5b9514
alyfazeha <alyfazeha@gmail.com>
2025-04-29 17:28:12 +0700

    submit final

---
+++ JOBSHEET9/Surat05.java
@@ -0,0 +1,25 @@
+package JOBSHEET9;
+public class Surat05 {
+    String idSurat;
+    String namaMahasiswa;
+    String kelas;
+    char jenisIzin;
+    int durasi;
+    boolean statusCek;
+
+    Surat05() {
+    }
+
+    Surat05(String idSurat, String namaMahasiswa, String kelas, char jenisIzin, int durasi) {
+        this.idSurat = idSurat;
+        this.namaMahasiswa = namaMahasiswa;
+        this.kelas = kelas;
+        this.jenisIzin = jenisIzin;
+        this.durasi = durasi;
+        this.statusCek = false;
+    }
+
+    void suratDicek() {
+        this.statusCek = true;
+    }
+}
