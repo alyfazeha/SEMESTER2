3fb54398c0bff0cb8b9dab609f5a6865fadc60cf
alyfazeha <alyfazeha@gmail.com>
2025-04-29 13:52:58 +0700

    praktikum1

---
+++ JOBSHEET9/Mahasiswa05.java
@@ -0,0 +1,18 @@
+package JOBSHEET9;
+public class Mahasiswa05 {
+    String nama;
+    String nim;
+    String kelas;
+    int nilai;
+
+    Mahasiswa05(String nama, String nim, String kelas) {
+        this.nama = nama;
+        this.nim = nim;
+        this.kelas = kelas;
+        nilai = -1;
+    }
+
+    void tugasDinilai(int nilai) {
+        this.nilai = nilai;
+    }
+}
